38a74d84d0aa96916b88b1773460b907b21e2d35
kushiluv <kushiluvjangu@gmail.com>
2021-12-24 23:18:52 +0530

    orc collision 1

--- src/main/java/com/example/helloapplication/game.java
+++ src/main/java/com/example/helloapplication/game.java
@@ -1,18 +1,19 @@
 package com.example.helloapplication;
 
 
-import javafx.animation.Animation;
-import javafx.animation.PathTransition;
-import javafx.animation.TranslateTransition;
+import javafx.animation.*;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Bounds;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
@@ -26,23 +27,32 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 import java.util.Timer;
 
-public class game {
-
-
+public class game implements Initializable {
 
     @FXML
-    private ImageView chest1;
-
+    private Label counter;
+    @FXML
+    private Group chests;
+    @FXML
+    private ImageView defaultchest;
 
     @FXML
-    private ImageView orc;
+    private ImageView island1;
+
+
+
     public void start(Stage gamestage) throws IOException {
+
         Parent root = FXMLLoader.load(getClass().getResource("gamescene.fxml"));
+
         Scene scene = new Scene(root, 1200, 600);
         Hero hero = new Hero(scene);
+
         Green_Orc grorc = new Green_Orc(scene);
         menu menu = new menu(scene);
         gameoverwindow gameoverwindow = new gameoverwindow(scene);
@@ -50,10 +60,68 @@ public class game {
         menu.paneinvisible();
         gameoverwindow.gameoverpaneinvisible();
         gameoverwindow.quitpaneinvisible();
+        counter  = (Label) scene.lookup("#counter");
+        defaultchest = (ImageView) scene.lookup("#defaultchest");
+        island1 = (ImageView) scene.lookup("#island1");
+        final ImageView chest1 = new ImageView("wep_0006 #56893.png");
+        final ImageView chest2 = new ImageView("wep_0007 #37947.png");
+        final ImageView chest3 = new ImageView("wep_0008 #30876.png");
+        final ImageView chest4 = new ImageView("wep_0009 #57652.png");
+        chests = (Group) scene.lookup("#chests");
 
-        chest1 = (ImageView) scene.lookup("#chest1");
+//        chests.setTranslateX(903);
+//        chests.setTranslateY(313);
+        chests.setScaleX(0.35);
+        chests.setScaleY(0.35);
+//        chests.setVisible(false);
+        Timeline chestt = new Timeline();
+        chestt.setCycleCount(1);
 
-        orc = (ImageView) scene.lookup("#orc");
+        chestt.getKeyFrames().add(new KeyFrame(Duration.millis(200),
+                        new EventHandler<ActionEvent>() {
+                            @Override
+                            public void handle(ActionEvent event) {
+                                chests.getChildren().setAll(chest1);
+                            }
+                        }));
+        chestt.getKeyFrames().add(new KeyFrame(Duration.millis(600),
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        chests.getChildren().setAll(chest2);
+                    }
+                }));
+        chestt.getKeyFrames().add(new KeyFrame(Duration.millis(1000),
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        chests.getChildren().setAll(chest3);
+                    }
+                }));
+        chestt.getKeyFrames().add(new KeyFrame(Duration.millis(1400),
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        chests.getChildren().setAll(chest4);
+                    }
+                }));
+//        defaultchest.setTranslateX(900);
+//        defaultchest.setTranslateY(313);
+        defaultchest.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                defaultchest.setVisible(false);
+                chestt.play();
+                chestt.setOnFinished(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        chests.setVisible(false);
+
+                    }
+                });
+            }
+        });
+
 
         islands cislands = new islands(scene);
         TranslateTransition orcjump = new TranslateTransition();
@@ -67,11 +135,13 @@ public class game {
         hero.set_hero_fall(fall);
         fall.play();
 
+        final int[] dashc = {0};
+
         final int[] flag1 = {0};
         ArrayList<ImageView> gameelements = new ArrayList<>();
         gameelements.addAll(cislands.getIslands());
         gameelements.add(grorc.getHero());
-        gameelements.add(chest1);
+        gameelements.add(defaultchest);
 
 
 //
@@ -80,11 +150,11 @@ public class game {
             @Override
             public void handle(ActionEvent event) {
                 int flag = 0;
-                Bounds boundshero = hero.getHero().localToScene(hero.getHero().getBoundsInLocal());
+
 
                 ArrayList<Bounds> islands = new ArrayList<>();
                 islands.addAll(cislands.getALLislands());
-                if(boundshero.getMinY()>=600&& flag1[0] ==0){
+                if(hero.getLocation().getMinY()>=600&& flag1[0] ==0){
                     flag1[0] =1;
                     gameoverwindow.gameoverpanevisible();
 
@@ -108,8 +178,9 @@ public class game {
 
                 }
                 for(int i =0;i<islands.size();i++) {
-                    if (Math.abs(Math.floor(boundshero.getMaxY()) - Math.floor(islands.get(i).getMinY()))<=4 && islands.get(i).getMaxX() >= boundshero.getMinX() && islands.get(i).getMinX() <= boundshero.getMaxX()) {
-
+                    if (Math.abs(Math.floor(hero.getLocation().getMaxY()) - Math.floor(islands.get(i).getMinY()))<=4 && islands.get(i).getMaxX() >= hero.getLocation().getMinX() && islands.get(i).getMinX() <= hero.getLocation().getMaxX()) {
+//                        System.out.println("island: "+islands.get(0).getMaxX()+" "+islands.get(0).getMinX()+" "+islands.get(0).getMinY());
+//                        System.out.println("hero min x"+hero.getLocation().getMaxX());
                         fall.pause();
                         jump.play();
                         flag = 1;
@@ -155,8 +226,9 @@ public class game {
                         Bounds boundshero = hero.getHero().localToScene(hero.getHero().getBoundsInLocal());
                         Bounds boundsorc = grorc.getHero().localToScene(grorc.getHero().getBoundsInLocal());
                         TranslateTransition translate = new TranslateTransition();
-                        System.out.println("hero max : "+boundshero.getMaxX());
-                        System.out.println("orcc min : "+boundsorc.getMinX());
+
+//                        System.out.println("hero max : "+boundshero.getMaxX());
+//                        System.out.println("orcc min : "+boundsorc.getMinX());
                         if(boundsorc.getMinX()-125<boundshero.getMaxX()+100&&boundsorc.getMinX()-125>boundshero.getMaxX()){
 
                             System.out.println("colliding");
@@ -166,18 +238,29 @@ public class game {
                             translate.play();
 //                            Polyline path = new Polyline();
 //                            path.getPoints().addAll(new Double[]{-200.0,0.0,-100.0,0.0,100.0,500.0});
-
+                            ArrayList<Bounds> islands = new ArrayList<>();
+                            islands.addAll(cislands.getALLislands());
                             pt.setNode(grorc.getHero());
-                            pt.setByX(100);
+                            pt.setByX(150);
                             pt.setDuration(Duration.seconds(0.07));
+
+                            System.out.println("island1 "+islands.get(1).getMaxX()+" "+islands.get(1).getMinX()+" "+islands.get(1).getMinY());
+                            System.out.println("island2: "+islands.get(2).getMaxX()+" "+islands.get(2).getMinX()+" "+islands.get(2).getMinY());
+                            System.out.println("orc y : "+boundsorc.getMaxY()+"\n orc x : "+boundsorc.getMaxX());
+                            System.out.println("hero min x"+hero.getLocation().getMaxX());
+
 
 
                         }
                         else {
                             translate.setDuration(Duration.seconds(0.07));
                             translate.setByX(100);
+                            dashc[0]++;
+                            counter.setText(Integer.toString(dashc[0]));
+
                             translate.setNode(hero.getHero());
                             translate.play();
+
                         }
                         fall.play();
                         translate.setOnFinished(new EventHandler<ActionEvent>() {
@@ -203,23 +286,35 @@ public class game {
                                                 islands.addAll(cislands.getALLislands());
                                                 //System.out.println("orc y : "+boundsorc.getMaxY()+"\n orc x : "+boundshero.getCenterX());
                                                 //System.out.println("island y min : "+boundsisland1.getMinY()+"x : "+boundsisland1.getMinX()+" max: "+boundsisland1.getMaxX());
-                                                //for (int i = 0; i < islands.size(); i++) {
-////                                                    if (!(Math.abs(Math.floor(boundsorc.getMaxY()) - Math.floor(islands.get(i).getMinY())) <= 4 && islands.get(i).getMaxX() >= boundsorc.getMinX() && islands.get(i).getMinX() <= boundsorc.getMaxX())) {
-////                                                        TranslateTransition translat = new TranslateTransition();
-////                                                        translat.setDuration(Duration.seconds(2));
-////                                                        translat.setByY(600);
-////                                                        translat.setNode(grorc.getHero());
-////                                                        translat.play();
-////                                                        tflag = 1;
-////
-////                                                    }
-//                                                }
+                                                for(int i =0;i<islands.size();i++) {
+                                                    if (islands.get(i).getMaxX() -161 >= boundsorc.getMinX() && islands.get(i).getMinX()-161 <= boundsorc.getMaxX()) {
+                                                        tflag = 1;
+                                                        orcjump.play();
+                                                        break;
+
+
+
+                                                    }
+                                                }
+
                                                 if(cislands.getALLislands().get(1).intersects(grorc.getHero().getBoundsInParent())){
                                                     System.out.println("bitch on the island");
                                                 }
 
                                                if(tflag==0){
-                                                   orcjump.play();
+                                                   TranslateTransition orcfall = new TranslateTransition();
+                                                   orcfall.setDuration(Duration.seconds(1));
+                                                   orcfall.setByY(800);
+                                                   orcfall.setCycleCount(1);
+                                                   orcfall.setAutoReverse(false);
+                                                   orcfall.setNode(grorc.getHero());
+                                                   orcfall.play();
+                                                   orcfall.setOnFinished(new EventHandler<ActionEvent>() {
+                                                       @Override
+                                                       public void handle(ActionEvent event) {
+                                                           grorc.getHero().setVisible(false);
+                                                       }
+                                                   });
 
                                                }
                                             }
@@ -261,4 +356,46 @@ public class game {
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        final ImageView chest1 = new ImageView("wep_0006 #56893.png");
+//        final ImageView chest2 = new ImageView("wep_0007 #37947.png");
+//        final ImageView chest3 = new ImageView("wep_0008 #30876.png");
+//        final ImageView chest4 = new ImageView("wep_0009 #57652.png");
+//        chests.setTranslateX(200);
+//        chests.setTranslateY(220);
+//
+//        Timeline chestt = new Timeline();
+//        chestt.setCycleCount(Timeline.INDEFINITE);
+//
+//        chestt.getKeyFrames().add(new KeyFrame(Duration.millis(200),
+//                new EventHandler<ActionEvent>() {
+//                    @Override
+//                    public void handle(ActionEvent event) {
+//                        chests.getChildren().setAll(chest1);
+//                    }
+//                }));
+//        chestt.getKeyFrames().add(new KeyFrame(Duration.millis(400),
+//                new EventHandler<ActionEvent>() {
+//                    @Override
+//                    public void handle(ActionEvent event) {
+//                        chests.getChildren().setAll(chest2);
+//                    }
+//                }));
+//        chestt.getKeyFrames().add(new KeyFrame(Duration.millis(600),
+//                new EventHandler<ActionEvent>() {
+//                    @Override
+//                    public void handle(ActionEvent event) {
+//                        chests.getChildren().setAll(chest3);
+//                    }
+//                }));
+//        chestt.getKeyFrames().add(new KeyFrame(Duration.millis(800),
+//                new EventHandler<ActionEvent>() {
+//                    @Override
+//                    public void handle(ActionEvent event) {
+//                        chests.getChildren().setAll(chest4);
+//                    }
+//                }));
+//        chestt.play();
+    }
 }
--- src/main/java/com/example/helloapplication/islands.java
+++ src/main/java/com/example/helloapplication/islands.java
@@ -16,6 +16,14 @@ public class islands extends GameObject {
 
     @FXML
     private ImageView island2;
+    @FXML
+    private ImageView island3;
+
+    @FXML
+    private ImageView island4;
+
+    @FXML
+    private ImageView island5;
 
     private ArrayList<ImageView> islands;
     private ArrayList<Bounds> islands_bounds;
@@ -25,9 +33,15 @@ public class islands extends GameObject {
         island = (ImageView) scene.lookup("#island");
         island1 = (ImageView) scene.lookup("#island1");
         island2 = (ImageView) scene.lookup("#island2");
+        island3 = (ImageView) scene.lookup("#island3");
+        island4 = (ImageView) scene.lookup("#island4");
+        island5 = (ImageView) scene.lookup("#island5");
         islands.add(island);
         islands.add(island1);
         islands.add(island2);
+        islands.add(island3);
+        islands.add(island4);
+        islands.add(island5);
     }
 
     public ArrayList<ImageView> getIslands() {
@@ -42,10 +56,16 @@ public class islands extends GameObject {
         Bounds boundsisland = island.localToScene(island.getBoundsInLocal());
         Bounds boundsisland1 = island1.localToScene(island.getBoundsInLocal());
         Bounds boundsisland2 = island2.localToScene(island.getBoundsInLocal());
+        Bounds boundsisland3 = island3.localToScene(island.getBoundsInLocal());
+        Bounds boundsisland4 = island4.localToScene(island.getBoundsInLocal());
+        Bounds boundsisland5 = island5.localToScene(island.getBoundsInLocal());
         islands_bounds = new ArrayList<>();
         islands_bounds.add(boundsisland);
         islands_bounds.add(boundsisland1);
         islands_bounds.add(boundsisland2);
+        islands_bounds.add(boundsisland3);
+        islands_bounds.add(boundsisland4);
+        islands_bounds.add(boundsisland5);
         return islands_bounds;
     }
 
