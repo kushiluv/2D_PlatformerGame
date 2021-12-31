9bbcf2c0abb1b0fbdde4dccce18dd8bce82c3d98
kushiluv <kushiluvjangu@gmail.com>
2021-12-31 21:47:31 +0530

    coinsdonejd

--- src/main/java/com/example/helloapplication/SaveGame.java
+++ src/main/java/com/example/helloapplication/SaveGame.java
@@ -5,17 +5,17 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class SaveGame {
-    private static ArrayList<String>
-
-    class Serialize_obj{
-        private Hero hero;
-        private int coinc[];
-        private int dashc[];
-        public Serialize_obj(){
-
-        }
-    }
-
-    ArrayList<Object> save_objs = new ArrayList<>();
-    ObjectOutputStream ob = new ObjectOutputStream(new FileOutputStream())
+//    private static ArrayList<String>
+//
+//    class Serialize_obj{
+//        private Hero hero;
+//        private int coinc[];
+//        private int dashc[];
+//        public Serialize_obj(){
+//
+//        }
+//    }
+//
+//    ArrayList<Object> save_objs = new ArrayList<>();
+//    ObjectOutputStream ob = new ObjectOutputStream(new FileOutputStream())
 }
